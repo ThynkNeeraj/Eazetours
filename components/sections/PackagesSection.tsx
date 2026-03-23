686d8d2fc7f9ceebf6ad2166b80c59c89fd86ea7
ThynkNeeraj <thynk.neeraj@gmail.com>
2026-03-23 16:26:33 +0530

    Update PackagesSection.tsx

--- components/sections/PackagesSection.tsx
+++ components/sections/PackagesSection.tsx
@@ -59,7 +59,7 @@ const PackagesSection: React.FC<PackagesSectionProps> = ({ locale = "en" }) => {
 
       {/* Packages Grid */}
       <div className="flex justify-center items-center max-w-screen-xl mx-0 sm:mx-auto">
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8 w-full">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-10 w-full">
           {packagesList.map((tourPackage: IPackageDetailDataType) => (
             <PackageSummaryCard
               key={tourPackage.Id}
